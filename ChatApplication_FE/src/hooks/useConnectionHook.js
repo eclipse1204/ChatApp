fec8690d78fa6eed9a4f4ded54a281c54eeb863d
Juvaraj Bhattacharjee <juvaraj.bhattacharjee@antrepriz.com>
2026-05-13 01:16:48 +0530

    Changes

--- ChatApplication_FE/src/hooks/useConnectionHook.js
+++ ChatApplication_FE/src/hooks/useConnectionHook.js
@@ -4,27 +4,33 @@ import toast from 'react-hot-toast';
 
 function useConnectionHook() {
     const [connection,setConnection]=useState({
-        loading:true,
-        connected:false
+        loading:sessionStorage.getItem("loadingSession")?sessionStorage.getItem("loadingSession")==="true":true,
+        connected:sessionStorage.getItem("connectedSession")?sessionStorage.getItem("connectedSession")==="true":false,
     })
 
     useEffect(()=>{
-        get("/connection",(e,r)=>{
-            if(r){
-                setConnection({
-                    loading:false,
-                    connected:true
-                })
-            }
-            else if(e){
-                toast.error(e?.data?.message);
-                setConnection({
-                    loading:false,
-                    connected:false
-                })
-            }
-        })
-    },[])
+        if(connection?.loading){
+            get("/connection",(e,r)=>{
+                if(r){
+                    setConnection({
+                        loading:false,
+                        connected:true
+                    })
+                    sessionStorage.setItem("loadingSession",false)
+                    sessionStorage.setItem("connectedSession",true)
+                }
+                else if(e){
+                    toast.error(e?.data?.message);
+                    setConnection({
+                        loading:false,
+                        connected:false
+                    })
+                    sessionStorage.setItem("connectedSession",false)
+                }
+            })   
+        }
+    },[connection])
+    
 
     return connection;
 }
